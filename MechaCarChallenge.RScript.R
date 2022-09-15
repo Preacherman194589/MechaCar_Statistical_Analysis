07c0a31bbfc041fe8a1ad1f456e7d439f31b7760
Preacherman194589 <106892740+Preacherman194589@users.noreply.github.com>
2022-09-15 15:19:52 -0700

    Add files via upload

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,4 +1,19 @@
-lm(formula = vehicle_length ~ vehicle_weight + spoiler_angle + 
-     ground_clearance + AWD + mpg, data = MechaCar_mpg)
-summary(lm(formula = vehicle_length ~ vehicle_weight + spoiler_angle + 
-             ground_clearance + AWD + mpg, data = MechaCar_mpg))
+lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
+     ground_clearance + AWD, data = MechaCar_mpg)
+
+summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
+             ground_clearance + AWD, data = MechaCar_mpg))
+
+total_summary <-MechaCarChallenge.RScript%>% + summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+View(total_summary)
+
+lot_summary <- MechaCarChallenge.RScript %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+View(lot_summary)
+
+t.test(MechaCarChallenge.RScript$PSI, mu=1500)
+
+t.test(Lot1$PSI, mu=1500)
+
+t.test(Lot2$PSI, mu=1500)
+
+t.test(Lot3$PSI, mu=1500)
